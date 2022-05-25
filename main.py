893a821f0266a339bdd651c66b160fb1e665bfef
Pebpung <mcuwwa29@gmail.com>
2022-05-25 22:13:33 +0900

    refactor: 실험 세팅2 구성

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from monai.inferers import sliding_window_inference
 from functools import partial
 from train import run_training
 from dataloader import get_loader
+from adabelief_pytorch import AdaBelief
 import gc
 import os
 import torch
@@ -14,7 +15,9 @@ import torch.nn as nn
 import argparse
 import datetime as dt
 import wandb 
+import warnings
 
+warnings.filterwarnings('ignore')
 gc.collect()
 
 parser = argparse.ArgumentParser(description="UNETR segmentation pipeline")
@@ -22,9 +25,9 @@ parser.add_argument("--logdir", default="logs", type=str, help="directory to sav
 parser.add_argument("--data_dir", default="data/Task500_ATLAS", type=str, help="dataset directory")
 parser.add_argument("--json_list", default="dataset.json", type=str, help="dataset json file")
 parser.add_argument("--max_epochs", default=200, type=int, help="max number of training epochs")
-parser.add_argument("--batch_size", default=8, type=int, help="number of batch size")
-parser.add_argument("--optim_lr", default=2e-4, type=float, help="optimization learning rate")
-parser.add_argument("--workers", default=14, type=int, help="number of workers")
+parser.add_argument("--batch_size", default=2, type=int, help="number of batch size")
+parser.add_argument("--optim_lr", default=1e-4, type=float, help="optimization learning rate")
+parser.add_argument("--workers", default=4, type=int, help="number of workers")
 parser.add_argument("--roi_x", default=128, type=int, help="roi size in x direction")
 parser.add_argument("--roi_y", default=128, type=int, help="roi size in y direction")
 parser.add_argument("--roi_z", default=128, type=int, help="roi size in z direction")
@@ -33,6 +36,8 @@ parser.add_argument("--out_channels", default=1, type=int, help="number of outpu
 parser.add_argument("--wandb", action="store_true", help="start wandb")
 parser.add_argument("--noamp", action="store_true", help="do NOT use amp for training")
 parser.add_argument("--val_every", default=10, type=int, help="validation frequency")
+parser.add_argument("--norm_name", default="group", type=str, help="normalize name")
+parser.add_argument("--dropout_rate", default=0.1, type=float, help="dropout rate")
 
 def main():
     args = parser.parse_args()
@@ -56,9 +61,9 @@ def main():
         mlp_dim=3072,
         num_heads=12,
         pos_embed="perceptron",
-        norm_name="instance",
+        norm_name=args.norm_name,
         res_block=True,
-        dropout_rate=0.0,
+        dropout_rate=args.dropout_rate,
     )
 
     model = nn.DataParallel(model, output_device=0)
@@ -70,11 +75,15 @@ def main():
         wandb.init(config=vars(args), project="TASK02_ATLAS")
         wandb.watch(model)
 
-    epoch = 100
+    if args.optimizer == 'adamw':
+        optimizer = torch.optim.AdamW(model.parameters(), lr=args.optim_lr, weight_decay=1e-5)
+    elif args.optimizer == 'adabelief':
+        optimizer = AdaBelief(model.parameters(), lr=1e-3, eps=1e-16, betas=(0.9,0.999), weight_decouple = True, rectify = True)
+
     dice_loss = DiceCELoss(to_onehot_y=False, sigmoid=True)
-    optimizer = torch.optim.AdamW(model.parameters(), lr=1e-4, weight_decay=1e-5)
+    
     dice_metric = DiceMetric(include_background=True, reduction="mean", get_not_nans=True)
-    scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=epoch)
+    scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=args.epoch)
     post_pred = Compose([Activations(sigmoid=True), AsDiscrete(threshold_values=True)])
 
     model_inferer = partial(
